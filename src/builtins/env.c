8be3605d25fbf4fee2c61aa66f5554e499694aa0
Anahit K <anikakasamanyan@gmail.com>
2022-12-09 17:53:15 +0100

    envvv

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -1,6 +1,25 @@
 #include "../../include/minishell.h"
 
-bool	env(t_cmd *cmdnode)
+// bool	env(t_cmd *cmdnode)
+// {
+// 	int		i;
+// 	t_data	*data;
+
+// 	if (cmdnode->cmd_arr[1])
+// 		return (msg_error("env", E_MANYARG, NULL), true);
+// 	data = cmdnode->data;
+// 	if (!data->env)
+// 		return (msg_error("env", "not found", NULL), true);
+// 	i = 0;
+// 	while (data->env[i])
+// 	{
+// 		printf("%s\n", data->env[i]);
+// 		i++;
+// 	}
+// 	return (false);
+// }
+
+void	env_builtin(t_cmd *cmdnode)
 {
 	int		i;
 	t_data	*data;
@@ -13,8 +32,10 @@ bool	env(t_cmd *cmdnode)
 	i = 0;
 	while (data->env[i])
 	{
-		printf("%s\n", data->env[i]);
+		write(cmdnode->fd_out, data->env[1], ft_strlen(data->env[i]));
+		write(cmdnode->fd_out, "\n", 2);
+		// printf("%s\n", data->env[i]);
 		i++;
 	}
-	return (false);
+	// return (false);
 }
